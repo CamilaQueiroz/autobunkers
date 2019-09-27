fb2141b9737a1725cc0f7d6910a7483ebe2c4f4a
Diego Soares <diegosoaresit@gmail.com>
2019-09-27 15:02:18 -0300

    Alterando de CDN para node_modules - Bootstrap, Jquery, Popper

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'popper.js'
+
+
 ReactDOM.render(<App />, document.getElementById('root'));

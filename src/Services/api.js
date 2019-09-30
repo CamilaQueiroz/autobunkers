1ca7e03160fdf52092147ad363e685510fc71c38
Camila <camilasvqueiroz@gmail.com>
2019-09-30 15:08:57 -0300

    Api token

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = '0x7d655a59d3d9faa92aa47bef2c779ab4';
+const token = '0x1d1d1314d33925a7eb36817fdc43a347';
 
 const api = axios.create({
   baseURL: 'http://apisites.bndv.com.br',

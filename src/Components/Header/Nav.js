279d39a80c84e8ef5984f81562381308e437ef14
Diego Soares <diegosoaresit@gmail.com>
2019-10-04 13:32:59 -0300

    Atualização do menu

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -3,83 +3,57 @@ import React from 'react';
 export default function Nav() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
+      <nav className="navbar navbar-light shadow-md navbar-transparent navbar-expand-lg shadow-md sticky-top">
+        <a className="navbar-brand text-size--42" href="https://autobunkers.com.br/"><strong className="text-dark"><span className="text-secondary">Auto</span> Bunkers</strong></a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="nav navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Empresa
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Empresa
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Veículos
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Veículos
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Consignado
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Consignado
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Parceiros
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Parceiros
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Informações
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Informações
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contato
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contato
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <b>Bunkers +</b>
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider" />
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </li>
-            </ul>
-          </div>
+            </li>
+          </ul>
         </div>
       </nav>
     </>

89f76b02670e9f4a21fe334d1f13251cee43450d
Camila <camilasvqueiroz@gmail.com>
2019-10-04 16:48:51 -0300

    build teste

---
+++ build/precache-manifest.e8038160717bd5705711561494a2b859.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4416df3b770023ec5fd2022f75dee89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa8b3ab5d30a509893b",
+    "url": "/static/css/2.bc536066.chunk.css"
+  },
+  {
+    "revision": "177e72b9e3facb7d28ef",
+    "url": "/static/css/main.eaef88b9.chunk.css"
+  },
+  {
+    "revision": "daa8b3ab5d30a509893b",
+    "url": "/static/js/2.d5a090db.chunk.js"
+  },
+  {
+    "revision": "177e72b9e3facb7d28ef",
+    "url": "/static/js/main.88b5f51b.chunk.js"
+  },
+  {
+    "revision": "312c8cdeb1bf0305b503",
+    "url": "/static/js/runtime~main.cb81d4e1.js"
+  },
+  {
+    "revision": "a8c2f937aa6f7ae95746c2d78d5590a9",
+    "url": "/static/media/auto-bunkers-banner-home.a8c2f937.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "6990f3921ec78e5baa3584b7cbcb0e60",
+    "url": "/static/media/logo-site.6990f392.png"
+  },
+  {
+    "revision": "1280ba12e369a948483d627481e3956c",
+    "url": "/static/media/urus.1280ba12.jpg"
+  }
+]);
